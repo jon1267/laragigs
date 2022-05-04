1d56a5d6c2ddb03f6aa4fcb9f64bd45430bfd225
Jon Leev <jonleev@gmail.com>
2022-05-04 16:13:24 +0300

    filter via get params

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+
+    // по сути толково: в контроллере (см ListingController@index) в выборке данных
+    // добавляется ->filter(request(['tag']))->get() + эта функция и есть фильтр тэгов.
+    public function scopeFilter($query, array $filters)
+    {
+        if($filters['tag'] ?? false) {
+            $query->where('tags', 'like', '%' . request('tag') . '%');
+        }
+    }
 }
